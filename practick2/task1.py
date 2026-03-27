f85097f437aed8d5c22f9b9c17e506467b084502
Alexrior <sasadorodnikov39@gmail.com>
2026-03-27 13:35:53 +0500

    Add files via upload

---
+++ practick2/task1.py
@@ -0,0 +1,9 @@
+import re
+
+date_pattern = re.compile(r"20\d{2}-[0,1]\d{1}-[0-3]\d{1} [1,2]\d{1}:[0-5]\d{1}:[0-5]\d{1}")
+
+if __name__ == "__main__":
+    print("Введите текст:")
+    TEXT = input()
+    dates = date_pattern.findall(TEXT)
+    print(*dates, sep="\n")
