4e645b459f6af53b623fa13254d6b7987eb36d86
Alexrior <sasadorodnikov39@gmail.com>
2026-03-27 13:34:39 +0500

    Add files via upload

---
+++ practick1/Task2.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    N = int(input())
+    numbers = list(range(N, N**2))
+    unsquare = [pow(n, 0.5) for n in numbers] 
+
+    print(f"{numbers=}")
+    print(f"{unsquare=}")
